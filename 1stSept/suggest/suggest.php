6499c2104b6d2953b32bfae832dc61d69970390d
sruthi2498 <vsruthi98@gmail.com>
2018-09-05 18:44:39 +0530

    5th Sept

--- 1stSept/suggest/suggest.php
+++ 1stSept/suggest/suggest.php
@@ -1,5 +1,15 @@
 <?php
+extract($_GET);
+$file=fopen("food.txt","r");
+$res=array();
 
+while(!feof($file)){
+	$line=trim(fgets($file)); // read each line
+	if(strncasecmp($line, $term, strlen($term))==0){ //compare line and term
+		$res[]=$line;
+	}
+}
+echo json_encode($res);
 	
 ?>
 	
