182df422864bb73fbcf80b6762f08c71d59e1fec
sruthi2498 <vsruthi98@gmail.com>
2018-09-09 21:04:11 +0530

    changed made to 29thAug/29thAug_xhr_complexRequests.html and .php

--- 29thAug/29thAug_xhr.php
+++ 29thAug/29thAug_xhr.php
@@ -1,6 +1,6 @@
 <?php
 	header("Access-Control-Allow-Origin:*");
-	header("Access-Control-Allow-Methods:PUT");
+
 	//send json data
 	$res=array();
 	extract($_GET);
--- 29thAug/29thAug_xhr_complexRequests.php
+++ 29thAug/29thAug_xhr_complexRequests.php
@@ -1,37 +1,33 @@
-<html>
-	<head>
-		<script>	
+<?php
+	header("Access-Control-Allow-Origin:*");
+	header("Access-Control-Allow-Methods:PUT");
+	//send json data
+	$res=array();
+	extract($_GET);
+	
 
-			var obj={
-				xhr: new XMLHttpRequest(),
-				getDetails:function(){
-					//when readyState changes, call fn showFile
-					this.xhr.onreadystatechange=this.showFile;
+	if($fname=="pizza"){
+		$res["cs"]="Italian";
+		$res["price"]="200";
+		$res["veg"]="non-veg";
+	}
+	else if($fname=="cornflakes"){
+		$res["cs"]="Continental";
+		$res["price"]="500";
+		$res["veg"]="veg";
+	}
+	else if($fname=="dosa"){
+		$res["cs"]="Indian";
+		$res["price"]="100";
+		$res["veg"]="veg";
+	}
+	else if($fname=="akkiroti"){
+		$res["cs"]="South-Indian";
+		$res["price"]="150";
+		$res["veg"]="veg";
+	}
 
-					var fname=document.getElementById("fname").value;
-					this.xhr.open("PUT","http:http://localhost/class/29thAug/29thAug_xhr.php?fname="+fname,true);
-					this.xhr.send();
-				},
-				showFile:function showFile(){
-					//in showfile current object is obj.xhr
-					if(this.readyState==4 && this.status==200){
-
-						var res=this.responseText; //as text string
-						//convert to json object
-						var resJson=JSON.parse(res);
-						document.getElementById("cs").value=resJson.cs;
-						document.getElementById("price").value=resJson.price;
-						document.getElementById("veg").value=resJson.veg;
-					}
-				}
-			}
-		
-		</script>
-	</head>
-	<body>
-		Food <input type="text" name="fname" id="fname" onblur="obj.getDetails()"/><br>
-		Cuisine <input type="text" name="cs" id="cs"/><br>
-		Price<input type="text" name="price" id="price"/><br>
-		Veg <input type="text" name="veg" id="veg"/>
- 	</body>
-</html>
+	//json_encode 
+	$ret=json_encode($res);
+	echo $ret;
+?>
