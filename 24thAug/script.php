af4ed4c09ff0dd0a782cd0f10a648bedf7f047e7
sruthi2498 <vsruthi98@gmail.com>
2018-09-13 08:13:41 +0530

    11thSept addded call_apply.html

---
+++ 24thAug/script.php
@@ -0,0 +1,4 @@
+<?php
+	$text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec ut tortor ac neque interdum elementum. Etiam tellus dolor, cursus non eros at, eleifend gravida ligula. Vivamus elementum ex id quam sodales elementum. Fusce consectetur malesuada mi a sagittis. Praesent dictum massa sed felis aliquam, sed cursus felis finibus. Etiam sed est ante. Pellentesque sed iaculis erat. Nam dui turpis, convallis quis neque sit amet, placerat efficitur felis. Praesent at finibus nibh, ut euismod nisl. Nullam in luctus felis. In faucibus, ligula vitae malesuada luctus, ante augue tincidunt nibh, at rhoncus augue tortor eu ante.";
+	echo 'document.getElementById("display").innerHTML+="'.$text.'"';
+?>
